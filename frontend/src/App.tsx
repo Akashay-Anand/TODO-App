00ad55948557d7d897eca248345356a5aa4c0702
Akashay Anand <akashay.anand@jalantechnologies.com>
2023-11-14 13:59:48 +0530

    setup major components with Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,28 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Todo from './components/Todo';
+import Navbar from './components/Navbar';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <p>.</p>
     </>
   );
 }
